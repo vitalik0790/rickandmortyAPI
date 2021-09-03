1e170476d4b879e9000ef05549dd4ea8e94621bb
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-09-03 21:45:13 +0300

    starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Switch, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
