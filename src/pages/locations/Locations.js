6d697f231e15b77c2e65fe66470e73af8d320d9d
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-09-09 19:00:55 +0300

    locations

--- src/pages/locations/Locations.js
+++ src/pages/locations/Locations.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import s from './Locations.module.css';
 
 function Locations() {
     return (
-        <div className={s.container}>
+        <div>
 
         </div>
     );
